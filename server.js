00d643feb52635d83976535654a710ab79f721c4
Ariel Cook <arielcook101014@gmail.com>
2019-10-04 22:43:24 -0700

    commit changes

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
+var PORT = process.env.PORT || 5000;
+
 connection.connect(function(err) {
   if (err) {
     return console.error("error: " + err.message);
@@ -46,4 +48,4 @@ app.use((err, req, res, next) => {
   res.status(404).send("<h2>The path is not valid</h2>");
 });
 
-app.listen(5000, () => console.log("Server running..."));
+app.listen(PORT, () => console.log("Server running..."));

8c3523a01919c63f731d21a940beea75d886f4fd
Ariel Cook <arielcook101014@gmail.com>
2019-10-06 21:34:42 -0700

    fixed form to post data

--- routers/index.js
+++ routers/index.js
@@ -1,2 +1,83 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require("mysql");
+
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "My_mysql1",
+    database: "db2",
+    multipleStatements: true
+  });
+}
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to the MySQL server");
+});
+
+router.post("/car", function(req, res) {
+  let currentCount;
+  let post;
+  let make = req.body.Make;
+
+  function insertCar() {
+    const promise = new Promise((resolve, reject) => {
+      connection.query("SELECT COUNT(*) FROM Cars", function(error, results) {
+        if (error) {
+          return reject(error);
+        }
+        resolve(results);
+      });
+    });
+    return promise;
+  }
+
+  insertCar()
+    .then(res => {
+      currentCount = res[0]["COUNT(*)"]; // get number of records
+      post = { Id: currentCount + 1, Make: make };
+
+      connection.query("INSERT INTO Cars SET ?", post, function(
+        error,
+        results
+      ) {
+        if (error) throw error;
+      });
+    })
+    .catch(err => console.log("Ariel Error!!!!"));
+
+  //   connection.query("SELECT COUNT(*) FROM Cars", function(error, results) {
+  //     if (error) throw error;
+  //     currentCount = results[0]["COUNT(*)"]; // get number of records
+  //       post = { Id: parseInt(currentCount) + 1, Make: make };
+  //   });
+
+  //   post = { Id: 7, Make: "Mitsubishi7" };
+  //   connection.query("INSERT INTO Cars SET ?", post, function(
+  //     error,
+  //     results,
+  //     fields
+  //   ) {
+  //     if (error) throw error;
+  //   });
+
+  res.redirect("/");
+});
+
+router.get("/", function(req, res) {
+  connection.query("SELECT * FROM Cars", function(error, results, fields) {
+    if (error) throw error;
+    // console.log("The solution is: ", results);
+    //   res.json(results);
+    res.render("index", { data: results });
+  });
+});
+
+// connection.end();
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,24 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const mysql = require("mysql");
 const path = require("path");
+const index = require("./routers/index");
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // use middleware to parse form data
 
 app.use(cors());
-
-if (process.env.JAWSDB_URL) {
-  var connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "My_mysql1",
-    database: "db2"
-  });
-}
 
 var PORT = process.env.PORT || 5000;
 
@@ -30,66 +19,8 @@ app.set("view engine", "ejs");
 // set path for static assets
 app.use(express.static(path.join(__dirname, "public")));
 
-connection.connect(function(err) {
-  if (err) {
-    return console.error("error: " + err.message);
-  }
-
-  console.log("Connected to the MySQL server");
-
-  app.get("/", function(req, res) {
-    // let post = { Id: 5, Make: "Mitsubishi5" };
-    // connection.query("INSERT INTO Cars SET ?", post, function(
-    //   error,
-    //   results,
-    //   fields
-    // ) {
-    //   if (error) throw error;
-    //   // res.json(results.insertId);
-    // });
-
-    // connection.query(
-    //   "UPDATE Cars SET Make = ? WHERE Id = ?",
-    //   ["123", 1],
-    //   function(error, results, fields) {
-    //     if (error) throw error;
-    //   }
-    // );
-
-    // connection.query("SELECT * FROM Cars WHERE Id = 1", function(
-    //   error,
-    //   results,
-    //   fields
-    // ) {
-    //   if (error) throw error;
-    //   console.log("The solution is: ", results);
-    //   res.json(results);
-    // });
-
-    connection.query("SELECT * FROM Cars", function(error, results, fields) {
-      if (error) throw error;
-      console.log("The solution is: ", results);
-      res.json(results);
-    });
-  }); // end app.get()
-}); // end connection
-
-// connection.connect(function(err) {
-//   if (err) {
-//     return console.error("error: " + err.message);
-//   }
-
-//   console.log("Connected to the MySQL server.");
-//   app.get("/", function(req, res) {
-//     connection.query("SELECT * FROM Cars", function(error, results, fields) {
-//       if (error) throw error;
-//       console.log("The solution is: ", results);
-//       res.json(results);
-//     });
-//   });
-// });
-
-// connection.end();
+// routes
+app.use("/", index);
 
 // Custom middleware below to handle errors. This middleware takes precedence to next()
 app.use((err, req, res, next) => {
